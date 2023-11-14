6c0c7bd1b34498cfc5f4d6283940c1c603af1b22
Batjin Lamjav <l3atjin@gmail.com>
2023-11-13 17:09:50 -0800

    added rider driver feed structure

---
+++ src/components/feed/DriverFeed.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import SearchBar from "../SearchBar";
+
+export default function DriverFeed() {
+  return (
+    <View>
+      <SearchBar></SearchBar>
+    </View>
+  );
+}
--- src/screens/main/FeedScreen.tsx
+++ src/screens/main/FeedScreen.tsx
@@ -1,11 +1,18 @@
 import { View, StyleSheet } from "react-native";
 import React from "react";
-import SearchBar from "../../components/SearchBar";
+import { useUserType } from "../../context/UserTypeProvider";
+import DriverFeed from "../../components/feed/DriverFeed";
+import RiderFeed from "../../components/feed/RiderFeed";
 
 export function FeedScreen() {
+  const [userType] = useUserType();
   return (
     <View style={styles.container}>
-      <SearchBar></SearchBar>
+      {userType === "rider" ? (
+        <RiderFeed></RiderFeed>
+      ) : (
+        <DriverFeed></DriverFeed>
+      )}
     </View>
   );
 }
